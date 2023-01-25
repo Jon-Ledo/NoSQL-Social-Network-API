5c27994aa4fd5d2c8ea78267a6b6ca8ee441e965
JonLedo <jonledo.code@gmail.com>
2023-01-25 06:48:15 -0500

    wrote out lay out for all the thoughts controllers. updated some lines thru out. experiencing some bugs with the api. Function to create a reaction and delete reactions looks correct, but doesn't function correctly yet.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,7 +15,7 @@ router.route('/').get(getAllUsers).post(createNewUser)
 // api/users/:userId
 router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser)
 
-// **`/api/users/:userId/friends/:friendId`**
+// api/users/:userId/friends/:friendId
 router
   .route('/:userId/friends/:friendId')
   .post(addNewFriend)
